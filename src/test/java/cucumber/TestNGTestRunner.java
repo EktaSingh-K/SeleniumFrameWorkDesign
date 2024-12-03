193e24477a571386c4bcfc839f40b9c0b256590d
ekta <ekta128singh@gmail.com>
2024-12-04 02:20:28 +0530

    a comment added in runner file

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -6,6 +6,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features="src/test/java/cucumber",glue="rahulshettyacademy.stepDefinitions",
 monochrome=true, tags = "@Errorvalidation" ,plugin= {"html:target/cucumber.html"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
-
+// Changes
 	
 }
